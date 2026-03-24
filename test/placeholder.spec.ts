5dc359170f15296e93c55feeb83d81022709c245
Scranner <zeldas-link@hotmail.co.uk>
2026-03-25 00:26:24 +0900

    format files

--- test/placeholder.spec.ts
+++ test/placeholder.spec.ts
@@ -1,7 +1,7 @@
-import { expect, it, describe } from "vitest"
+import { expect, it, describe } from "vitest";
 
-describe('placeholder', () => {
-    it('passes', () => {
-        expect(true).toBeTruthy()
-    })
-})
+describe("placeholder", () => {
+	it("passes", () => {
+		expect(true).toBeTruthy();
+	});
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig, defaultExclude } from "vitest/config";
 
 export default defineConfig({
-    test: {
-        watch: false,
-        isolate: false,
-        exclude: defaultExclude.concat([
-            "**/dist/**",
-        ]),
-    },
+	test: {
+		watch: false,
+		isolate: false,
+		exclude: defaultExclude.concat(["**/dist/**"]),
+	},
 });
